ad4c8ab3be3147e2efdadce34f6f298246d679fb
Adrian <adriantoddross@gmail.com>
2018-04-06 15:18:22 -0400

    add optimized function & comment out first attempt

--- palindrome.js
+++ palindrome.js
@@ -10,32 +10,50 @@ function is_palindrome(string) {
 
   for (let i = 0; i < string.length; i++) {
     stack.push(string[i]);
+    if (string[i] !== stack.pop(string[i])) {
+      return false;
+    } else {
+      return true;
+    }
   }
-
-  for (let i = 0; stack.top !== null; i++) {
-    chars += stack.pop(string[i]);
-  }
-
-  if (string === chars) {
-    return true;
-  } else {
-    return false;
-  }
-
 }
 
-console.log(is_palindrome('1001'));
+console.log(is_palindrome('dad'));
 
-// display(stack);
-// make the string characters lowercase
-// initialize the new stack
-// open a for loop to split string into characters: s[i]
-// inside for loop:
-// push characters into the stack - stack.push(s[i]) - d a d
-// exit for loop
-//enter for loop
-// pop the letters off of the stack and into a new variable
-// pop until this.top === null
-// exit for loop
-// compare the strings
-// return boolean
+// function is_palindrome2(string) {
+
+//   let stack = new Stack();
+//   let chars = '';
+//   string = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+
+//   for (let i = 0; i < string.length; i++) {
+//     stack.push(string[i]);
+//   }
+
+//   for (let i = 0; stack.top !== null; i++) {
+//     chars += stack.pop(string[i]);
+//   }
+
+//   if (string === chars) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+
+// }
+
+// console.log(is_palindrome2('dad'));
+
+/* PSEUDOCODE:
+make the string characters lowercase
+initialize the new stack
+open a for loop to split string into characters: s[i]
+inside for loop:
+push characters into the stack - stack.push(s[i]) - d a d
+exit for loop
+enter for loop
+pop the letters off of the stack and into a new variable
+pop until this.top === null
+exit for loop
+compare the strings
+return true or false */
