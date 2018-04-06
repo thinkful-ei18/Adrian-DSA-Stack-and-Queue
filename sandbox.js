7ec21a0e0bc0fa47d35f61dd3b6ef2c8cc4e9b72
Adrian <adriantoddross@gmail.com>
2018-04-06 11:37:09 -0400

    push 4 names into stack

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,16 @@
 'use strict';
 
+const Stack = require('./stack');
+
 const main = (function () {
 
+  let stack = new Stack;
+  // Kirk, Spock, McCoy, Scotty
+  stack.push('Kirk');
+  stack.push('Spock');
+  stack.push('McCoy');
+  stack.push('Scotty');
+  console.log(stack);
 
 })();
 
-main();
