b899fc344aa00daffb207e1a9d91a9237fb30537
Adrian <adriantoddross@gmail.com>
2018-04-06 12:13:28 -0400

    add & test display()

--- sandbox.js
+++ sandbox.js
@@ -13,14 +13,32 @@ const main = (function () {
     return console.log(JSON.stringify(stack.top.data), 'is at the top of the stack');
   }
 
+  function display(stack) {
+    if (stack.top === null) {
+      throw new Error('The stack is empty');
+      // Throw an error if the stack is empty!
+    }
+
+    let currentNode = stack.top;
+    let data = '';
+    while(currentNode) {
+      data += `=> ${currentNode.data} `;
+      currentNode = currentNode.next;
+    }
+    return console.log('CURRENT STACK', data);
+  }
+
   let stack = new Stack;
   // Kirk, Spock, McCoy, Scotty
   stack.push('Kirk');
   stack.push('Spock');
   stack.push('McCoy');
   stack.push('Scotty');
-  console.log(JSON.stringify(stack));
   peek(stack);
+  display(stack);
+  stack.pop();
+  stack.pop();
+  display(stack);
 
 })();
 
