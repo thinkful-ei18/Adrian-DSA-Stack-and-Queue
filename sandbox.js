facf3cd33b1de17ddffca49e5144f0098ae557e4
Adrian <adriantoddross@gmail.com>
2018-04-06 11:57:00 -0400

    finish peek()

--- sandbox.js
+++ sandbox.js
@@ -4,13 +4,23 @@ const Stack = require('./stack');
 
 const main = (function () {
 
+  function peek(stack) {
+    if (stack.top === null) {
+      throw new Error('The stack is empty');
+      // Throw an error if the stack is empty!
+    }
+
+    return console.log(JSON.stringify(stack.top.data), 'is at the top of the stack');
+  }
+
   let stack = new Stack;
   // Kirk, Spock, McCoy, Scotty
   stack.push('Kirk');
   stack.push('Spock');
   stack.push('McCoy');
   stack.push('Scotty');
-  console.log(stack);
+  console.log(JSON.stringify(stack));
+  peek(stack);
 
 })();
 
